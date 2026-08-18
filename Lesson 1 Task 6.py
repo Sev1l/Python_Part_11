bac7ea73162071467d28ddc14cb1ec12fc7ff54c
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:46:02 +0400

    Update Lesson 1 Task 6.py

--- Lesson 1 Task 6.py
+++ Lesson 1 Task 6.py
@@ -2,9 +2,9 @@
 # Task 6 (Begin with a vowel)
 
 def begin_with_vowel(words: list):
-    return [word for word in words if word[0].lower() in 'a,e,i,o,u' ]
+    return [word for word in words if word and word[0].lower() in 'aeiou']
 
 
-word_list = ["automobile","motorbike","Animal","cat","Dog","APPLE","orange"]
+word_list = ["automobile", "motorbike", "Animal", "cat", "Dog", "APPLE", "orange"]
 for vowelled in begin_with_vowel(word_list):
     print(vowelled)
