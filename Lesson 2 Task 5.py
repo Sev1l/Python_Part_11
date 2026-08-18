9f3b38727040bf6b2bd6362df87a2a8b760e1d77
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:49:58 +0400

    Update Lesson 2 Task 5.py

--- Lesson 2 Task 5.py
+++ Lesson 2 Task 5.py
@@ -1,7 +1,6 @@
 ## More comprehensions
 # Task 5 (Most common words)
 
-
 def most_common_words(filename: str, lower_limit: int):
     dict = {}
     with open (filename) as f:
@@ -12,7 +11,7 @@ def most_common_words(filename: str, lower_limit: int):
             if i not in dict and list.count(i) >= lower_limit:
                 dict[i] = list.count(i)
         print(dict)
-        
-
+    return dict
 
 most_common_words("comprehensions.txt", 3)
+
