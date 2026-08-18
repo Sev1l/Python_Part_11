4242869d0ade5656bc9b7d08c571cda6ecb1fe9b
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:47:16 +0400

    Update Lesson 2 Task 1.py

--- Lesson 2 Task 1.py
+++ Lesson 2 Task 1.py
@@ -2,10 +2,11 @@
 # Task 1 (Filter forbidden)
 
 def filter_forbidden(string: str, forbidden: str):
-    new_str = ''.join([word for word in string if word not in forbidden])
+    new_str = ''.join([char for char in string if char not in forbidden])
     return new_str
 
 
 sentence = "Once! upon, a time: there was a python!??!?!"
 filtered = filter_forbidden(sentence, "!?:,.")
 print(filtered)
+
